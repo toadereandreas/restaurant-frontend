d582d0a7410cd34a21790be5e77682e77abd5826
toadereandreas <57907218+toadereandreas@users.noreply.github.com>
2021-01-15 17:48:11 +0200

    Done with about & reservation

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import Background from '../Images/menu_wallpaper.jpg';
-import team1 from '../Images/baracca__echipa1.png';
+import team1 from '../Images/baracca__echipa12.jpg';
 import team2 from '../Images/baracca__echipa2.png';
 import team3 from '../Images/baracca__echipa3.png';
 import Header from "./header";
@@ -18,14 +18,12 @@ class About extends Component {
         return (
             <section style={ sectionStyle }>
                 <Header/>
-                <div style={{ height: '800px', width: '80%', paddingLeft: '10%', backgroundColor: 'white', marginLeft: '10%', borderRadius: '15px', textAlign: 'center', margin: 'auto' }}>
+                <div style={{ paddingTop: '2%', fontSize: '30px', color: 'white', height: '800px', width: '80%', paddingLeft: '10%', backgroundColor: 'transparent', marginLeft: '10%', borderRadius: '15px', textAlign: 'center', margin: 'auto' }}>
                     Profesionalismul, dedicaţia şi dinamica întregii echipe, sunt elementele ce definesc colectivul BARACCA.
 
                     Rezultatul muncii de aici este o bucătărie contemporană care susţine viaţa sănătoasă şi experienţele care hrănesc în aceaşi măsură trupul şi sufletul.
 
                     <img src ={team1}/>
-                    <img src ={team2}/>
-                    <img src ={team3}/>
                 </div>
             </section>
         );
